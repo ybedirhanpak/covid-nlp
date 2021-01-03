1903e0b031be8efa22fbf01cc0103b7e667304bd
Yahya Bedirhan Pak <y.bedirhanpak@gmail.com>
2021-01-03 14:21:22 +0300

    metadata.py: Remove calculation of tf df

--- metadata.py
+++ metadata.py
@@ -13,9 +13,7 @@ def read_metadata(vocabulary: set):
     print("Preprocessing metadata.csv...")
     start_time = time.time()
 
-    term_frequency_doc = {}
     inverted_index = {}
-    document_frequency = {}
     normalized_documents = []
 
     with open(utils.get_file_path("data/metadata.csv"), 'r') as readFile:
@@ -33,9 +31,6 @@ def read_metadata(vocabulary: set):
             doc_content = utils.normalize_doc(
                 doc_title + ' ' + doc_abstract)
 
-            # Term frequency in this document
-            doc_term_frequency = {}
-
             normalized_doc = []
 
             # Preprocess each token in the document
@@ -49,52 +44,33 @@ def read_metadata(vocabulary: set):
 
                 normalized_doc.append(token)
 
-                # Add token to term frequency
-                if n_token not in doc_term_frequency:
-                    doc_term_frequency[n_token] = 1
-                else:
-                    doc_term_frequency[n_token] += 1
-
                 # Add document into inverted index
                 if n_token not in inverted_index:
                     inverted_index[n_token] = {doc_id}
                 elif doc_id not in inverted_index[n_token]:
                     inverted_index[n_token].add(doc_id)
-                    # Increase document frequency of token
-                    if n_token not in document_frequency:
-                        document_frequency[n_token] = 1
-                    else:
-                        document_frequency[n_token] += 1
 
                 # Add token to vocabulary
                 vocabulary.add(n_token)
 
-            term_frequency_doc[doc_id] = doc_term_frequency
             normalized_documents.append(' '.join(normalized_doc))
 
     readFile.close()
 
-    print("Pickle term_frequency_doc, document_frequency, inverted_index, normalized_documents")
+    print("Pickle inverted_index, normalized_documents")
 
     # Create out directory
     if not os.path.exists("out"):
         os.makedirs("out")
 
-    # Pickle term_frequency_doc
-    with open(utils.get_file_path("out/term_frequency_doc.pickle"), 'wb') as file:
-        pickle.dump(term_frequency_doc, file, protocol=pickle.HIGHEST_PROTOCOL)
-
     # Picke inverted_index
     with open(utils.get_file_path("out/inverted_index.pickle"), 'wb') as file:
         pickle.dump(inverted_index, file, protocol=pickle.HIGHEST_PROTOCOL)
 
-    # Picke document_frequency
-    with open(utils.get_file_path("out/document_frequency.pickle"), 'wb') as file:
-        pickle.dump(document_frequency, file, protocol=pickle.HIGHEST_PROTOCOL)
-
     # Picke normalized_documents
     with open(utils.get_file_path("out/normalized_documents.pickle"), 'wb') as file:
-        pickle.dump(normalized_documents, file, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(normalized_documents, file,
+                    protocol=pickle.HIGHEST_PROTOCOL)
 
     end_time = time.time()
 
